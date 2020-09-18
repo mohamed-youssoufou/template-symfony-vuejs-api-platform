de2430668e7c69924fa5824b57ae85c233dc1088
Calm <brice.lassissi@gmail.com>
2020-09-18 16:59:59 +0000

    fin filtre date

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -14,7 +14,10 @@
             <date-picker v-model="date1" valueType="format" ></date-picker>
           </div>
           <div class="col-lg-2">
-            <date-picker v-model="date2" valueType="format"></date-picker>
+            <date-picker v-model="date2" valueType="format" :default-value="new Date()"></date-picker>
+          </div>
+          <div class="col-lg-2">
+            <button class="btn btn-primary" @click="searchByTwoDates">Chercher</button>
           </div>
         </div>
         <table id="news-table" class="table tablesorter mb-5">
@@ -23,6 +26,7 @@
               <th scope="col" class="header">USER ID</th>
               <th scope="col" class="header">ID</th>
               <th scope="col" class="header">title</th>
+              <th scope="col" class="header">date</th>
               <th scope="col" class="header">completed</th>
               <td>actions</td>
             </tr>
@@ -32,6 +36,7 @@
               <td class="align-middle">{{todo.userId}}</td>
               <td class="align-middle">{{todo.id}}</td>
               <td class="align-middle">{{todo.title}}</td>
+              <td class="align-middle">{{todo.date}}</td>
               <td class="align-middle">{{todo.completed}}</td>
               <td class="align-middle">
                 <router-link to="/" class="btn btn-sm btn-primary">+</router-link>
@@ -82,7 +87,7 @@
         customLabels,
         searchTitle : "",
         date1: null,
-        date2: null,
+        date2: null
       }
     },
     computed: {
@@ -101,13 +106,47 @@
     methods : {
       onChangePage(pageOfItems) {
           this.pageOfItems = pageOfItems;
-        }
+        },
+        searchByTwoDates: function(){
+          
+          this.todos = this.todosFilter.filter(todo => {
+
+
+            let dateDiffWithDate1 = this.getTimeDiff(todo.date, this.date1)
+            let dateDiffWithDate2 = this.getTimeDiff(this.date2, todo.date)
+
+            if(
+              (dateDiffWithDate1 > 0 && dateDiffWithDate2 > 0)
+              || (dateDiffWithDate1 == 0 || dateDiffWithDate2 == 0)
+            ){
+              return todo
+            }
+
+          })
+
+          //if(this.date2 !== null && this.date2 !== null){
+          //  const diff = diff(this.date2, this.date1);
+          //  if(diff ==)
+          //}
+      },
+      getTimeDiff: (time1, time2) => {
+        return new Date(time1).getTime() - new Date(time2).getTime()
+      }
     },
     async mounted(){
       const res = await api.getTodos()
       this.$store.commit('TodosModule/SET_TODO', res.data)
       this.spinner = false
+      //test add date to todo
       this.todos = res.data
+      let newTodo = []
+      this.todos.forEach(todo => {
+        todo.date = `2020-9-${Math.floor(Math.random() * (30 - 1 + 1)) + 1}`
+        newTodo.push(todo)
+      });
+      this.todos = newTodo
+      //fin test
+      //this.todos = res.data
       this.todosFilter = this.todos
     }
   }
